f833fd52d60cde0cb7913e1f21fff12a6d3fc623
Surafel Tad <surafelbekele978@gmail.com>
2023-10-04 07:19:51 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import "../src/components/Style.css";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import {Routes, Route, Link} from "react-router-dom";
+import HowToRegIcon from '@mui/icons-material/HowToReg';
+import InputIcon from '@mui/icons-material/Input';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App=()=>{
+  return( <>
+
+<nav>
+    <ul>
+    
+        <li className="nav-todo"> <Link to="/signup"> <HowToRegIcon/> SignUp </Link></li>
+        <li className="nav-todo"> <Link to="/"><InputIcon/> Login </Link> </li>
+
+    </ul>
+  </nav>
+
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+      </>
+  )
 }
-
 export default App;
